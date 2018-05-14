d21fe0f4471d8682496e1809bf102276fc5932c0
alison <alison.americo@gmail.com>
2018-05-14 13:34:07 -0300

    fixing

--- consultre_proj/settings.py
+++ consultre_proj/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+#configuração
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'consultre_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
